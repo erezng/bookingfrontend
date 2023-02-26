a95753184f31dc658853b2d00da17fd30594a5b1
erez gabay <erez2g@gmail.com>
2023-02-26 17:08:13 +0200

    user saved alert

--- src/routes/Register.tsx
+++ src/routes/Register.tsx
@@ -43,12 +43,12 @@ const Register = () => {
       .register(username, email, password)
       .then((res) => {
         console.log(res.data);
+        alert("user saved!"); //swal //modal
         //swal
         nav("/login");
       })
       .catch((e) => {
         console.log(e);
-        alert(e); //swal //modal
         setErrMessage(JSON.stringify(e.response.data));
       })
       .finally(() => {
